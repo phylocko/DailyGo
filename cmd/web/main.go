d3f9eca26e4c10710f64337a0dd9bfb2e81a4b2f
Vladislav Pavkin <phylocko@mail.ru>
2022-12-22 20:20:56 +0300

    Day two: gorm

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"dailygo/internal/database"
+	"dailygo/internal/models"
 	"dailygo/internal/web/admin"
 	"dailygo/internal/web/api"
 
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
+func runServer() {
 	r := gin.Default()
 
 	r.LoadHTMLGlob("templates/*.html")
@@ -21,7 +23,28 @@ func main() {
 	{
 		adminRoutes.GET("/", admin.IndexView)
 		adminRoutes.GET("/info", admin.InfoView)
+
+		adminRoutes.GET("/resources", admin.ResourceListView)
+		adminRoutes.POST("/resources", admin.ResourceListView)
+
+		adminRoutes.GET("/resources/:id", admin.ResourceView)
+		adminRoutes.POST("/resources/:id", admin.ResourceView)
 	}
 
 	r.Run()
+}
+
+// Migrate creates a database structure.
+// Call it manually if needed.
+func Migrate() {
+	database.Db.AutoMigrate(
+		&models.Resource{},
+		&models.IpAddress{},
+	)
+}
+
+func main() {
+	database.CreateConnection()
+	runServer()
+
 }
